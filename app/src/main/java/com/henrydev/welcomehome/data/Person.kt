52c7a30ae3ed1c7e98d4b9dd42dd40bbcabc92f2
Henry Salazar <hjsmipuc@gmail.com>
2025-11-25 20:25:34 -0500

    Creation of entities

---
+++ app/src/main/java/com/henrydev/welcomehome/data/Person.kt
@@ -0,0 +1,43 @@
+package com.henrydev.welcomehome.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.Relation
+
+@Entity(tableName = "persons")
+data class Person(
+    @PrimaryKey(autoGenerate = true)
+    val personId: Int = 0,
+    @ColumnInfo(name = "first_name")
+    val firstName: String,
+    @ColumnInfo(name = "last_name")
+    val lastName: String,
+    @ColumnInfo(name = "cell_phone")
+    val cellphone: Int,
+    @ColumnInfo(name = "residential_complex")
+    val residentialComplex: String,
+    @ColumnInfo(name = "created_at")
+    val createdAt: Long = System.currentTimeMillis(),
+    val rolId: Int
+)
+
+@Entity(tableName = "rol")
+data class Rol(
+    @PrimaryKey
+    val rolId: Int,
+    val name: String
+)
+
+//member o visitor
+
+data class RolWithPerson(
+    @Embedded val rol: Rol,
+    @Relation(
+        parentColumn = "rolId",
+        entityColumn = "rolId"
+    )
+    val persons: List<Person>
+)
+
